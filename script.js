122d0d320535bfe560c6162265992b346821a579
Peter Bruckner <peterbruckner08@gmail.com>
2021-03-19 14:52:32 +1030

    set up arrays, and first prompt

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
- //setting up prompts/confirms 
- var hasNumbers = prompt("How many characters would you like?", "Character Amount?");
- var specCharacters = confirm("Would you like special characters?", "Special Characters?");
- var confNumbers = confirm("Would you like Numbers?", "Numbers?");
- var confLowercase = confirm("Would you like Lowercase letters?", "Lowercase?"); 
- var confUpperCase = confirm("Would you like Uppercase letters", "Uppercase?");
- // setting up arrays
- var lowerLetters = "abcdefghijklmonpqrstuvwxyz".split("")
- var upperLetters = "abcdefghijklmonpqrstuvwxyz".toUpperCase().split("")
- var numbers = "1234567890".split("")
- var specialCharacters = "!@#$%^&*()".split("")
- var final = []
-
-
- // Assignment Code
+  // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -22,13 +8,32 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-generatePassword(){
-  
+function generatePassword() {
+
+ //setting up prompts/confirms 
+ var charAmount = Number(window.prompt("How many characters would you like?", "Character Amount?"));
+ 
+ if (charAmount < 8 || charAmount > 129) {
+confirm("Password must contain 8-128 characters")
+charAmount = Number(window.prompt("How many characters would you like?"))
+ }
+// **TODO**add loop
+
+ var specCharacters = confirm("Would you like special characters?", "Special Characters?");
+ var confNumbers = confirm("Would you like Numbers?", "Numbers?");
+ var confLowercase = confirm("Would you like Lowercase letters?", "Lowercase?"); 
+ var confUpperCase = confirm("Would you like Uppercase letters", "Uppercase?");
+ // setting up arrays
+
+ var lowerLetters = "abcdefghijklmonpqrstuvwxyz".split("")
+ var upperLetters = "abcdefghijklmonpqrstuvwxyz".toUpperCase().split("")
+ var numbers = "1234567890".split("")
+ var specialCharacters = "!@#$%^&*()".split("")
+ var final = []
+
+
 }
-
-
-
